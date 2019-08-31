52e412968a43a6a8b0277ac04a508882fd86006a
cassia franco <cassia.developer@gmail.com>
2019-08-31 17:42:11 -0300

    Initial Commit

---
+++ fotoid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fotoid
+//
+//  Created by Cassia Franco on 31/08/2019.
+//  Copyright © 2019 Cassia Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
